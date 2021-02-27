31cb78f4994e5f25fb82b8d967bec77d956c1e97
AlfilAlex <cabimax@outlook.com>
2021-02-27 00:06:08 -0600

    Ya vamos avanzanodo

--- coordinador2_tkinter.py
+++ coordinador2_tkinter.py
@@ -5,82 +5,87 @@ import tkinter as tk
 
 
 class iniciadora(tk.Frame):
-  def __init__(self, parent):
-    tk.Frame.__init__(self, parent)
-    self.initialize()
+    def __init__(self,parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
 
-    label = tk.Label(self, text="Productos_de_hoy")
-    label.grid(row=0, column=0)
+    def initialize(self):    
+        self.parent.title('Venta_de_hoy')
+        self.parent.geometry('500x400')
 
-    #Botones -----------    -----------------   -------------
-    self.boton_archivo = Button(self, text='Agregar_productos', command=self.lista_de_productos)
-    self.boton_archivo.grid(row=1, column=0, padx=20, pady=30)
+        self.frame.grid(row=0, column = 0)
 
-    self.boton_agregar = Button(self, text='Nuevo producto', command=self.agregar_producto)
-    self.boton_agregar.grid(row=2, column=0, padx=20, pady=30)
+        label = tk.Label(self, text="Productos_de_hoy")
+        label.grid(row=0, column=0)
 
-    #Cuadros de texto ----------    -----------------   -----
-    self.outputtxt = Text(self, height = 10, width = 25, bg = "light yellow")
-    self.outputtxt.grid(row=1, column=1, padx=20, pady=30)
+        #Botones -----------    -----------------   -------------
+        boton_archivo = Button(self, text='Agregar_productos', command=self.lista_de_productos)
+        boton_archivo.grid(row=1, column=0, padx=20, pady=30)
 
-    self.inputtxt = Text(self, height = 10, width = 25, bg = "light yellow")
-    self.inputtxt.grid(row=2, column=1, padx=20, pady=30)
-    
-    #Info de la clase ---- ---- --- --- --- ---- --- ---- ---- ---- 
-    self.conexion = None
-    self.list_Producto = []
+        boton_agregar = Button(self.frame, text='Nuevo producto', command=self.agregar_producto)
+        boton_agregar.grid(row=2, column=0, padx=20, pady=30)
 
-  def lista_de_productos(self):    
-    cursor = self.conecta_base()
-    cursor.execute("""SELECT PRODUCTOS FROM productos""")
-    self.list_Producto = cursor.fetchall()   
-    
-  
-  def agregar_producto(self):
-    inputtxt = self.inputtxt.get("1.0", "end-1c")
-    producto_to_dab = inputtxt.split(',')
-    #Trabajo con la db
-    cursor = self.conecta_base()
-    cursor.execute("INSERT INTO productos (PRODUCTOS, PRECIO) VALUES (?, ?)", producto_to_dab)
-    self.conexion.commit()
-    cursor.execute("""SELECT PRODUCTOS FROM productos""")
-    self.list_Producto = cursor.fetchall()
+        #Cuadros de texto ----------    -----------------   -----
+        outputtxt = Text(self.frame, height = 10, width = 25, bg = "light yellow")
+        outputtxt.grid(row=1, column=1, padx=20, pady=30)
 
-    self.outputtxt.delete('1.0', tk.END)
-    for producto in self.list_Producto:
-      self.outputtxt.insert(tk.END, producto[0] + '\n')
-    self.inputtxt.delete('1.0', tk.END)
+        inputtxt = Text(self.frame, height = 10, width = 25, bg = "light yellow")
+        inputtxt.grid(row=2, column=1, padx=20, pady=30)
+        
+        #Info de la clase ---- ---- --- --- --- ---- --- ---- ---- ---- 
+        self.conexion = None
+        self.list_Producto = []
 
-    
-  #Coneccion con la base
-  def conecta_base(self):
-    self.conexion = sqlite3.connect('productos.db')
-    cursor = self.conexion.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS productos  
-                    (ID_PROD INTEGER PRIMARY KEY  AUTOINCREMENT, 
-                    PRODUCTOS VARCHAR(15),
-                    PRECIO INT)""")
-    return cursor
+    def lista_de_productos(self):    
+        cursor = self.conecta_base()
+        cursor.execute("""SELECT PRODUCTOS FROM productos""")
+        self.list_Producto = cursor.fetchall()   
+      
 
-  def desconectar_base(self):
-    self.conexion.close()
+    def agregar_producto(self):
+        inputtxt = self.inputtxt.get("1.0", "end-1c")
+        producto_to_dab = inputtxt.split(',')
+        #Trabajo con la db
+        cursor = self.conecta_base()
+        cursor.execute("INSERT INTO productos (PRODUCTOS, PRECIO) VALUES (?, ?)", producto_to_dab)
+        self.conexion.commit()
+        cursor.execute("""SELECT PRODUCTOS FROM productos""")
+        self.list_Producto = cursor.fetchall()
+
+        self.outputtxt.delete('1.0', tk.END)
+        for producto in self.list_Producto:
+          self.outputtxt.insert(tk.END, producto[0] + '\n')
+        self.inputtxt.delete('1.0', tk.END)
+
+      
+    #Coneccion con la base
+    def conecta_base(self):
+        self.conexion = sqlite3.connect('productos.db')
+        cursor = self.conexion.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS productos  
+                        (ID_PROD INTEGER PRIMARY KEY  AUTOINCREMENT, 
+                        PRODUCTOS VARCHAR(15),
+                        PRECIO INT)""")
+        return cursor
+
+    def desconectar_base(self):
+        self.conexion.close()
 
 
-  def venta_dia(self):
-    self.conexion = sqlite3.connect('productos.db')
-    cursor = self.conexion.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS venta_dia  
-                    (ID_PROD INTEGER PRIMARY KEY  AUTOINCREMENT, 
-                    DIA DATE,
-                    VENTA INT)""")
-                    
-    numero_total = cursor.execute("""SELECT PRODUCTOS FROM productos""")
-    venta_hoy = []
-    cursor.execute("INSERT INTO productos (DIA, VENTA) VALUES (?, ?)", venta_hoy)
+    def venta_dia(self):
+        self.conexion = sqlite3.connect('productos.db')
+        cursor = self.conexion.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS venta_dia  
+                        (ID_PROD INTEGER PRIMARY KEY  AUTOINCREMENT, 
+                        DIA DATE,
+                        VENTA INT)""")
+                        
+        numero_total = cursor.execute("""SELECT PRODUCTOS FROM productos""")
+        venta_hoy = []
+        cursor.execute("INSERT INTO productos (DIA, VENTA) VALUES (?, ?)", venta_hoy)
 
-def iniciador_clase():
-  objeto_iniciador = iniciadora()
-  return objeto_iniciador
+
 
 if __name__ == '__main__':
   root = Tk()
--- parte_grafica.py
+++ parte_grafica.py
@@ -3,38 +3,65 @@
 #Agregar una venta para un dia determinado
 #Agregar una cantidad incial para un día determiando
 
-from tkinter import Tk, Button, Frame
+from tkinter import Tk, Button, Frame, Text
 from coordinador import iniciadora
+from inicio import mainthing
+from tkinter import ttk
+
+from funciones_db import ventas
+
 
 clase_preparadora = 'clase_en_proceso'
 
 root = Tk()
-root.geometry('500x300')
+root.geometry('600x300')
 root.title('Venta')
 
 frame = Frame(root, width=500, height=300)
 frame.grid(row=0, column=0)
 
+frame2 = Frame(frame, bg = 'red')
+frame2.grid(row=0, column=2, padx=20, pady=30)
 
 def venta():
+    prod_id = venta_Prod.get()
+    sell_Num = venta_Num.get()
+    ventas(prod_id, sell_Num)
     print('Se hizo una venta')
     pass
 
 def iniciador():
-  root = Tk()
-  root.geometry('500x400')
-  root.title('Venta_de_hoy')
-  while True:
-    iniciador = iniciadora(root).grid(row=0, column=0)
+    root = Tk()
+    app = mainthing(root)
     root.mainloop()
     
+#Seccion de venta
+venta_Prod = ttk.Combobox(frame, state="readonly")
+venta_Prod["values"] = ["Dulce", "Mole", "Rajas_queso", "Rojos"]
+venta_Prod.grid(row=0, column=0, padx=20, pady=30) #, padx=20, pady=30
+
+venta_Num = ttk.Combobox(frame)
+venta_Num["values"] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+venta_Num.grid(row=0, column=1)
+
+boton_sumVenta = Button(frame2, text='Sumar venta', command=venta)
+boton_sumVenta.grid(row=0, column=0)
+
+boton_resVenta = Button(frame2, text='Restar venta', command=venta)
+boton_resVenta.grid(row=0, column=1)
 
 
-boton_archivo = Button(frame, text='Se hizo una venta', command=venta)
-boton_archivo.grid(row=1, column=0, padx=20, pady=30)
 
 boton_archivo = Button(frame, text='Preparar_Programa', command=iniciador)
-boton_archivo.grid(row=0, column=0, padx=20, pady=30)
+boton_archivo.grid(row=3, column=0, padx=20, pady=30)
+
+#
+# combox3 = ttk.Combobox(frame, state="readonly")
+# combox3["values"] = ["Python", "C", "C++", "Java"]
+# combox3.grid(row=2, column=0, padx=20, pady=30)
+
+# outputtxt = Text(frame, height = 10, width = 25, bg = "light yellow")
+# outputtxt.grid(row=2, column=1, padx=20, pady=30)
 
 root.mainloop()
 
